d8ac8c9490dd11d6bcbd661cfc219e51fc3e6c5d
K-solar <164323925+K-solar@users.noreply.github.com>
2024-05-10 16:27:44 +0900

    Add files via upload

---
+++ springversion/rev/src/main/java/com/itbank/model/predictDAO.java
@@ -0,0 +1,16 @@
+package com.itbank.model;
+
+import org.apache.ibatis.annotations.Select;
+
+import com.itbank.model.vo.TableVO;
+
+public interface predictDAO {
+
+	@Select("select predict_value from #{table_name} where idx = #{month}")
+	int search(TableVO tv);
+	
+	// #{table_name} -> board -> 'board'
+	
+	
+	// ${table_name} -> board
+}
